fe3ef9bd7a2aac6b200d08f290d894eebd95256b
gpi <gpimpale29@gmail.com>
2025-09-06 01:34:40 -0700

    better typing

--- src/nano_hud_python/agent.py
+++ src/nano_hud_python/agent.py
@@ -4,14 +4,27 @@ import litellm
 from litellm import GenericResponseOutputItem, OutputFunctionToolCall, ResponsesAPIResponse
 from openai.types.responses import (
     FunctionToolParam,
+    ResponseCodeInterpreterToolCall,
     ResponseComputerToolCall,
+    ResponseCustomToolCall,
+    ResponseFileSearchToolCall,
     ResponseFunctionToolCall,
     ResponseFunctionToolCallParam,
+    ResponseFunctionWebSearch,
     ResponseInputItemParam,
     ResponseOutputMessage,
     ResponseOutputMessageParam,
+    ResponseReasoningItem,
+    ResponseReasoningItemParam,
 )
 from openai.types.responses.response_input_item_param import FunctionCallOutput
+from openai.types.responses.response_output_item import (
+    ImageGenerationCall,
+    LocalShellCall,
+    McpApprovalRequest,
+    McpCall,
+    McpListTools,
+)
 from pydantic import BaseModel
 
 from .environment import Environment
@@ -49,22 +62,22 @@ class Agent(BaseModel):
             )
             assert isinstance(response, ResponsesAPIResponse)
 
+            # store the tool call results till all tool calls are completed
             tool_call_results: list[FunctionCallOutput] = []
 
             for item in response.output:
                 match item:
-                    # openai types
                     case ResponseOutputMessage() | GenericResponseOutputItem():
                         self.input.append(ResponseOutputMessageParam(**item.model_dump()))
+                    case ResponseReasoningItem():
+                        self.input.append(ResponseReasoningItemParam(**item.model_dump()))
                     case ResponseFunctionToolCall() | OutputFunctionToolCall():
                         self.input.append(ResponseFunctionToolCallParam(**item.model_dump()))
 
-                        # assert the required fields are not None
                         assert item.name is not None
                         assert item.arguments is not None
                         assert item.call_id is not None
 
-                        # call the tool
                         result = await environment.call_tool(item.name, json.loads(item.arguments))
                         tool_call_results.append(
                             FunctionCallOutput(
@@ -73,14 +86,24 @@ class Agent(BaseModel):
                                 output=json.dumps(result.structuredContent),
                             )
                         )
-                    case ResponseComputerToolCall():
-                        raise NotImplementedError("Computer tool calls are not supported yet")
-                    case _:
-                        raise ValueError(f"Unknown item type: {item}")
+                    case (
+                        ResponseComputerToolCall()
+                        | ResponseFileSearchToolCall()
+                        | ResponseFunctionWebSearch()
+                        | ImageGenerationCall()
+                        | ResponseCodeInterpreterToolCall()
+                        | LocalShellCall()
+                        | McpCall()
+                        | McpListTools()
+                        | McpApprovalRequest()
+                        | ResponseCustomToolCall()
+                    ):
+                        raise NotImplementedError(f"Tool call {item.model_dump_json()} is not supported yet")
+                    case dict():
+                        raise NotImplementedError(f"Unknown item type: {json.dumps(item)}")
 
-            # add the tool call results to the input
+            # add the tool call results to the input now that the outputs are all added
             self.input.extend(tool_call_results)
 
-            # if there were no tool calls, break
             if len(tool_call_results) == 0:
                 break
